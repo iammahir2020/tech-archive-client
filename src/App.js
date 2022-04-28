ff9706bc5996131d542c883ca75bb8b88c9cd6b8
iammahir2020 <1810659@iub.edu.bd>
2022-04-29 04:32:47 +0600

    basic route setup done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Routes } from "react-router-dom";
+import Header from "../src/Components/Shared/Header/Header";
+import Footer from "../src/Components/Shared/Footer/Footer";
+import Home from "../src/Components/Pages/Home/Home";
+import Login from "../src/Components/Pages/Login/Login";
+import Register from "../src/Components/Pages/Register/Register";
+import Blogs from "../src/Components/Pages/Blogs/Blogs";
+import Inventory from "../src/Components/Pages/Inventory/Inventory";
+import ManageInventory from "../src/Components/Pages/ManageInventory/ManageInventory";
+import AddItem from "../src/Components/Pages/AddItem/AddItem";
+import MyItem from "../src/Components/Pages/MyItem/MyItem";
+import PageNotFound from "./Components/Pages/PageNotFound/PageNotFound";
 
 function App() {
   return (
     <div>
-      <h2>App.js</h2>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/inventory/:id" element={<Inventory></Inventory>}></Route>
+        <Route
+          path="/manage"
+          element={<ManageInventory></ManageInventory>}
+        ></Route>
+        <Route path="/add" element={<AddItem></AddItem>}></Route>
+        <Route path="/myItems" element={<MyItem></MyItem>}></Route>
+        <Route path="/blogs" element={<Blogs></Blogs>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/register" element={<Register></Register>}></Route>
+        <Route path="*" element={<PageNotFound></PageNotFound>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
