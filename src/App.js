ad4ded12aae07f242a0b4f54e0627b0322d7263f
iammahir2020 <1810659@iub.edu.bd>
2022-04-29 07:25:55 +0600

    header section added to home page

--- src/App.js
+++ src/App.js
@@ -13,24 +13,47 @@ import AddItem from "../src/Components/Pages/AddItem/AddItem";
 import MyItem from "../src/Components/Pages/MyItem/MyItem";
 import PageNotFound from "./Components/Pages/PageNotFound/PageNotFound";
 import { HelmetProvider } from "react-helmet-async";
+import RequireAuth from "./Components/Authentication/RequireAuth/RequireAuth";
 
 function App() {
   return (
-    <div>
+    <div className="app-container">
       <Header></Header>
       <HelmetProvider>
         <Routes>
           <Route path="/" element={<Home></Home>}></Route>
           <Route
             path="/inventory/:id"
-            element={<Inventory></Inventory>}
+            element={
+              <RequireAuth>
+                <Inventory></Inventory>
+              </RequireAuth>
+            }
           ></Route>
           <Route
             path="/manage"
-            element={<ManageInventory></ManageInventory>}
+            element={
+              <RequireAuth>
+                <ManageInventory></ManageInventory>
+              </RequireAuth>
+            }
           ></Route>
-          <Route path="/add" element={<AddItem></AddItem>}></Route>
-          <Route path="/myItems" element={<MyItem></MyItem>}></Route>
+          <Route
+            path="/add"
+            element={
+              <RequireAuth>
+                <AddItem></AddItem>
+              </RequireAuth>
+            }
+          ></Route>
+          <Route
+            path="/myItems"
+            element={
+              <RequireAuth>
+                <MyItem></MyItem>
+              </RequireAuth>
+            }
+          ></Route>
           <Route path="/blogs" element={<Blogs></Blogs>}></Route>
           <Route path="/login" element={<Login></Login>}></Route>
           <Route path="/register" element={<Register></Register>}></Route>
--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -1,10 +1,79 @@
+import logo from "../../../images/logoLight.png";
 import React from "react";
+import { Button, Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { signOut } from "firebase/auth";
 import "./Header.css";
+import auth from "../../../firebase.init";
 
 const Header = () => {
+  const [user, loading, error] = useAuthState(auth);
+
+  const handleLogOut = () => {
+    signOut(auth);
+  };
   return (
     <div>
-      <h2>Header</h2>
+      <Navbar
+        fixed="top"
+        bg="dark"
+        variant="dark"
+        className="nav-bar"
+        expand="lg"
+      >
+        <Container>
+          <Navbar.Brand as={NavLink} to="/">
+            <img className="header-logo" src={logo} alt="" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto nav-links">
+              <Nav.Link as={NavLink} to="/">
+                Home
+              </Nav.Link>
+              {user && (
+                <>
+                  <Nav.Link as={NavLink} to="/add">
+                    Add Item
+                  </Nav.Link>
+                  <Nav.Link as={NavLink} to="/manage">
+                    Manage Inventory
+                  </Nav.Link>
+                </>
+              )}
+              {user ? (
+                <>
+                  <NavDropdown
+                    title={user?.displayName}
+                    id="basic-nav-dropdown"
+                    className="user-logout"
+                  >
+                    <NavDropdown.Item as={NavLink} to="/myItems">
+                      My Items
+                    </NavDropdown.Item>
+                    <NavDropdown.Item
+                      onClick={handleLogOut}
+                      className="btn-logout"
+                    >
+                      Logout
+                    </NavDropdown.Item>
+                    {/* <button onClick={handleLogOut} className="btn-logout">
+                      Logout
+                    </button> */}
+                  </NavDropdown>
+                </>
+              ) : (
+                <>
+                  <Nav.Link as={NavLink} to="/login">
+                    <Button variant="light">Login</Button>
+                  </Nav.Link>
+                </>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   );
 };
